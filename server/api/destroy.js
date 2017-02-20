e9f587428775cd8da55e652e9fe36a2b0ad3d123
shaunpersad <shaunpersad@gmail.com>
2017-02-19 19:59:46 -0500

    Apocalypse!

--- server/api/destroy.js
+++ server/api/destroy.js
@@ -46,7 +46,7 @@ function destroy(req, res) {
                 const harvestUserEmail = _.get(harvestDelinquent, 'user.email', '');
 
                 const copyLink = req.app.locals.services.appUrl('copy');
-                const message = `${harvestDelinquent.getName()}, you have not logged 40 hours this week on harvest! You can either <${harvestLink}|use a blank timesheet>, or <${copyLink}|copy hours from last week>.\n\nDo it! Or I will subscribe you to cat facts forever.`;
+                const message = `${harvestDelinquent.getName()}, you have not logged 40 hours this week on harvest. <${harvestLink}|Use a blank timesheet>, or <${copyLink}|copy hours from last week>.\n\nDo this, human. Time is of the essence!`;
 
                 slack.messageUserByEmail(harvestUserEmail, message, slackUsers, (err) => {
 
