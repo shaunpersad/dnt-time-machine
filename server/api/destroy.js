6c16fb69721fbff86ff0d97e54f8aaa22d570dee
shaunpersad <shaunpersad@gmail.com>
2017-05-08 09:51:05 -0400

    wtf

--- server/api/destroy.js
+++ server/api/destroy.js
@@ -72,7 +72,7 @@ function destroy(req, res) {
         },
         (reallyBadPeople, next) => {
 
-            const badMessage = `${reallyBadPeople.join(', ')} didn't do their timesheets, so they will be left stranded on an island. Not together. On separate islands. Like in an island chain. And the water will be full of vindictive sharks, like in that movie with Serena from Gossip Girl.`;
+            const badMessage = `${reallyBadPeople.join(', ')} are morally corrupt and will never emotionally fulfill you, ever. Don't call me "honey". Who gon' check me, boo?`;
             const goodMessage = 'OMGWTFBBQ EVERYONE SUBMITTED THEIR TIMESHEETS!';
             const message = reallyBadPeople.length ? badMessage : goodMessage;
 
